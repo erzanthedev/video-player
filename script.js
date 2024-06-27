8c6b3d80ba4aeee02bd0620811febb8119409267
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-06-27 20:14:49 +1000

    feat: added play and pause logic

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-
+// Variables for DOM Manipulation ----------------------------------- //
+const video = document.querySelector("video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById(".volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".time-duration");
+const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play"); // Changes pause icon
+  playBtn.setAttribute("title", "Play");
+}
 
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause"); // Changes play icon
+    playBtn.setAttribute("title", "Pause");
+  } else {
+    video.pause();
+  }
+  showPlayIcon();
+}
+// On video End, show play button icon
+playBtn.addEventListener("ended", showPlayIcon);
 
-
+function pauseVideo() {
+  video.pause();
+  playBtn.classList.replace("fa-pause", "fa-play");
+}
 // Progress Bar ---------------------------------- //
-
-
 
 // Volume Controls --------------------------- //
 
-
-
 // Change Playback Speed -------------------- //
-
-
 
 // Fullscreen ------------------------------- //
 
-
+// Event Listeners
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
